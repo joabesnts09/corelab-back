10f1ab83c5899644093b0aac79d6d361871bfee0
joabesnts09 <joabesilva1233@gmail.com>
2025-06-26 17:06:48 -0300

    feat: add Prisma settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common'
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { TasksModule } from './modules/tasks/tasks.module';
-
+import { UsersModule } from './modules/users/users.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { TasksModule } from './modules/tasks/tasks.module'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './database/prisma.service'
 
 @Module({
-  imports: [UsersModule, AuthModule, TasksModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  
+],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
